18301a8d7d295b739b7269faa8afae304d1652dd
Janimarton <janimarton22@gmail.com>
2019-02-27 12:58:04 +0100

    student class initialized

---
+++ src/main/java/com/codecool/jpaintro/Student.java
@@ -0,0 +1,19 @@
+package com.codecool.jpaintro;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Student {
+
+
+
+}
