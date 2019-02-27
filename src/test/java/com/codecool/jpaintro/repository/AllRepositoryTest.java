f02deeb2c33226d715b8d3650b4d33c2c0b4614c
Janimarton <janimarton22@gmail.com>
2019-02-27 13:56:02 +0100

    email now: can NOT be null, but unique test wrote: saveUniqueFieldTwice() about 2 emails

--- src/test/java/com/codecool/jpaintro/repository/AllRepositoryTest.java
+++ src/test/java/com/codecool/jpaintro/repository/AllRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -37,6 +38,23 @@ public class AllRepositoryTest {
         List<Student> studentList = studentRepository.findAll();
         assertThat(studentList).hasSize(1);
 
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void saveUniqueFieldTwice(){
+        Student student = Student.builder()
+                .email("john@codecool.com")
+                .name("John")
+                .build();
+
+        studentRepository.save(student);
+
+        Student student2 = Student.builder()
+                .email("john@codecool.com")
+                .name("Peter")
+                .build();
+
+        studentRepository.saveAndFlush(student2);
     }
 
 }
