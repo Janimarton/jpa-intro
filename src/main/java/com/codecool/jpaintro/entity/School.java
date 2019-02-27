8a2f85bfd0ed2547280e5cb325522a9553476c83
Janimarton <janimarton22@gmail.com>
2019-02-27 14:50:58 +0100

    school initialized & Location enum (with Budapest and Miskolc)

---
+++ src/main/java/com/codecool/jpaintro/entity/School.java
@@ -0,0 +1,27 @@
+package com.codecool.jpaintro.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class School {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Location location;
+
+}
